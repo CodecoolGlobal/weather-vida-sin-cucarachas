86d3a401905fd108fe6b85c5bb1c76404b052e1d
TiborGulyas <gulyastibor@gmail.com>
2020-06-29 16:59:04 +0200

    default city data aquired

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CityTable from "./component/CityTable";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <div>
+                <CityTable />
+              </div>
+            )}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- weather/src/index.js
+++ weather/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { WeatherContextProvider } from "./context/WeatherContext";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WeatherContextProvider>
+      <App />
+    </WeatherContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
