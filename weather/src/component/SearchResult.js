c2feb6e0fefdece7ae57b4b2ce3d57c558668ba9
TiborGulyas <gulyastibor@gmail.com>
2020-06-30 19:57:03 +0200

    search result display - works

--- weather/src/component/SearchResult.js
+++ weather/src/component/SearchResult.js
@@ -1,8 +1,18 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { SearchContext } from "../context/SearchContext";
+import DisplayCity from "./DisplayCity";
+import axios from "axios";
 
 export default function SearchResult() {
   const [searchedString] = useContext(SearchContext);
+  const [resultCities, setResultCities] = useState([]);
 
-  return <div>{searchedString}</div>;
+  useEffect(() => {
+    const url = `https://www.metaweather.com/api/location/search/?query=${searchedString}`;
+    axios.get(url).then((res) => setResultCities(res.data));
+  }, [searchedString]);
+
+  console.log(resultCities);
+
+  return resultCities.map((city) => <DisplayCity city={city} />);
 }
