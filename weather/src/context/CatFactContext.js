7d0c1721a3dc01d5c27abb1b27850a2a73f8ed3a
Viktor Bocz <beviktor95@gmail.com>
2020-07-01 10:26:25 +0200

    Co-authored-by: steven-trautmann <steven.trautmann1@gmail.com>

--- weather/src/context/CatFactContext.js
+++ weather/src/context/CatFactContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from "react";
 export const CatFactContext = createContext();
 
 export const CatFactProvider = (props) => {
-  const [catFactData, setCatFactData] = useState("");
+  const [catFactData, setCatFactData] = useState("Click on me(ow) for a Fact!");
 
   return (
     <CatFactContext.Provider value={[catFactData, setCatFactData]}>
