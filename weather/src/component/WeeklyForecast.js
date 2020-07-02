4d7073407dc155665865379e20430d31c2642e34
aron <pachl.aron@gmail.com>
2020-07-02 18:34:01 +0200

    deleted commented lines

--- weather/src/component/WeeklyForecast.js
+++ weather/src/component/WeeklyForecast.js
@@ -86,32 +86,3 @@ const WeeklyForecast = (props) => {
   );
 };
 export default WeeklyForecast;
-
-{
-  /* <h2>{title}</h2>
-    {weather.map((day, index) => (
-      <div key={index + title}>
-        <h3>{`Current Temperature: ${Math.ceil(
-          day.the_temp * tempAd + tempAd32
-        )} ${tempUnit}`}</h3>
-        <h3>{"Current Weather:" + day.weather_state_name}</h3>
-        <h3>{"Date: " + day.applicable_date}</h3>
-        <img
-          src={`https://www.metaweather.com/static/img/weather/${day.weather_state_abbr}.svg`}
-          alt={day.weather_state_abbr}
-          width="64"
-          height="64"
-        />
-        <h3>{`Wind Speed: ${Math.ceil(
-          day.wind_speed * speedAd
-        )}${speedUnit}`}</h3>
-        <h3>{"Wind Direction: " + day.wind_direction}</h3>
-        <h3>{`Min temperature: ${Math.ceil(
-          day.min_temp * tempAd + tempAd32
-        )}${tempUnit}`}</h3>
-        <h3>{`Max temperature: ${Math.ceil(
-          day.max_temp * tempAd + tempAd32
-        )}${tempUnit}`}</h3>
-        <div>=============================</div>
-      </div> */
-}
